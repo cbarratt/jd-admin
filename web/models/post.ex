a37ad44ac3b86fd6a659f838c18dac3dfd434849
Callum Barratt <callum.barratt@sky.uk>
2016-05-25 20:48:27 +0100

    Added very basic API for posts

--- web/models/post.ex
+++ web/models/post.ex
@@ -3,21 +3,16 @@ defmodule JdAdmin.Post do
 
   schema "posts" do
     field :title, :string
+    field :slug, :string
     field :excerpt, :string
     field :body, :string
 
     timestamps
   end
 
-  @required_fields ~w(title excerpt body)
+  @required_fields ~w(title slug excerpt body)
   @optional_fields ~w()
 
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
